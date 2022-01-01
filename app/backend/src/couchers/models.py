79b7439055499b52f0b650b9d09393ec221fc0de
Aapeli <git@aapelivuorinen.com>
2022-01-01 20:24:18 +0200

    Add email notification stuff back

--- app/backend/src/couchers/models.py
+++ app/backend/src/couchers/models.py
@@ -536,10 +536,7 @@ class ContributorForm(Base):
 
         We currently send if expertise is listed, or if they list a way to help outside of a set list
         """
-        return (
-            (self.expertise != None)
-            | (not set(self.contribute_ways).issubset(set(["community", "blog", "other"])))
-        )
+        return (self.expertise != None) | (not set(self.contribute_ways).issubset(set(["community", "blog", "other"])))
 
 
 class SignupFlow(Base):
